34c2b6c30c207329df1ab7b4a86430f9f6f35312
neckutrek <>
2022-05-22 12:04:17 +0200

    update

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -4,12 +4,18 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install tmux git neovim curl powerline apt-file
+sudo apt install tmux git neovim curl powerline apt-file mlocate
+
+sudo apt install binutils gcc clang
+sudo apt install maven openjdk-8-dbg
 
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo update
 sudo apt install code
+
+sudo apt update
+sudo apt autoremove
 
 # Setup bashrc
 echo "source ~/.bashrc.priv" >> ~/.bashrc
@@ -22,4 +28,12 @@ echo "setxkbmap -option caps:swapescape" >> ~/.profile
 # Setup git
 git config --global user.email '<>'
 git config --global user.name 'neckutrek'
+
+# Setup tmux
+echo "set -g mouse on
+set -sg escape-time 0
+new
+neww
+splitw -h
+splitw -v" >> ~/.tmux.conf
 
