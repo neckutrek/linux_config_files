059a314b3177c2b8f3bf2126b43b799f50619f06
neckutrek <>
2022-05-22 15:12:30 +0200

    update

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Install packages
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
 sudo apt update
 sudo apt upgrade
 
-sudo apt install tmux git neovim curl powerline apt-file mlocate
+sudo apt install tmux git neovim curl powerline apt-file mlocate htop
 
-sudo apt install binutils gcc clang
-sudo apt install maven openjdk-8-dbg
+sudo apt install binutils gcc clang cmake
+sudo apt install maven openjdk-8-dbg openjdk-8-jdk-headless
+sudo apt install pip
+sudo apt install fzy ripgrep nodejs npm
 
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
@@ -16,6 +21,18 @@ sudo apt install code
 
 sudo apt update
 sudo apt autoremove
+
+# Install conan
+sudo pip install conan
+
+# Neovim setup
+pip install --upgrade pynvim
+sudo npm install -g neovim
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p ~/.config/nvim
+cp ./init.vim ~/.config/nvim/init.vim
 
 # Setup bashrc
 echo "source ~/.bashrc.priv" >> ~/.bashrc
@@ -30,10 +47,4 @@ git config --global user.email '<>'
 git config --global user.name 'neckutrek'
 
 # Setup tmux
-echo "set -g mouse on
-set -sg escape-time 0
-new
-neww
-splitw -h
-splitw -v" >> ~/.tmux.conf
-
+cp .tmux.conf ~/.tmux.conf
